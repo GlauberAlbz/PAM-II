a801a085747e0e3f977b224371d7773bc791c7ad
Glauber <glauberalmeidab@gmail.com>
2026-03-10 20:33:50 -0300

    Exercicio 8 finalizado

---
+++ src/ex8.kt
@@ -0,0 +1,11 @@
+/*
+Autor: Glauber Almeida de Brito
+Exercício 8: Crie uma variável numérica e exiba uma contagem regressiva até 0
+ */
+
+fun main() {
+    val num = 10
+    for (i in num downTo 0){
+        println("$i")
+    }
+}
