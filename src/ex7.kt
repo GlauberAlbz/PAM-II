f37c00fb04c43d6ac9436061a0bd7c771e929a69
Glauber <glauberalmeidab@gmail.com>
2026-03-10 20:30:09 -0300

    Criação do repositório

--- src/ex7.kt
+++ src/ex7.kt
@@ -1,19 +1,20 @@
 /*
 Autor: Glauber Almeida de Brito
-Exercício 8: Dado um array de números, calcule somente a soma dos números pares (ex: [1,2,3,4] -> 6
+Exercício 7: Dado um array de números, calcule somente a soma dos números pares (ex: [1,2,3,4] -> 6
  */
 
 fun main(){
     val lista = mutableListOf<Int>()
+    var soma = 0
     for (i in 1 .. 4) {
         print("Digite o ${i}º número: ")
         lista.add(readln().toInt())
     }
-    val lista_pares = mutableListOf<Int>()
-    for (i in 1 .. 4) {
-        if (lista[i] % 2 == 0){
-            lista.add(lista_pares)
+    for (i in lista) {
+        if (i % 2 == 0){
+            soma += i
         }
     }
+    println("A soma dos números pares é $soma")
 
 }
